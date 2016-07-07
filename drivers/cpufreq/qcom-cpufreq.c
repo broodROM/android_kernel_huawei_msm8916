TheTypoMaster/emotion_beta_501

Voltage Control: generic voltage control for DTS based kernels
Thanks to faux123

Signed-off-by: placiano <placiano80@gmail.com>
Signed-off-by: Pafcholini <nadyaivanova14@gmail.com>

drivers/cpufreq/qcom-cpufreq.c
@@ -35,6 +35,10 @@
 #include <trace/events/power.h>
 #include <mach/msm_bus.h>
 
+#ifdef CONFIG_CPU_VOLTAGE_CONTROL
+static struct cpufreq_frequency_table *dts_freq_table;
+#endif
+
 #ifdef CONFIG_DEBUG_FS
 #include <linux/debugfs.h>
 #include <linux/seq_file.h>
@@ -592,6 +596,20 @@ static int cpufreq_parse_dt(struct device *dev)
 	freq_table[i].driver_data = i;
 	freq_table[i].frequency = CPUFREQ_TABLE_END;
 
+#ifdef CONFIG_CPU_VOLTAGE_CONTROL
+    dts_freq_table =
+    devm_kzalloc(dev, (nf + 1) *
+                 sizeof(struct cpufreq_frequency_table),
+                 GFP_KERNEL);
+    
+    if (!dts_freq_table)
+        return -ENOMEM;
+    
+    for (i = 0, j = 0; i < nf; i++, j += 3)
+        dts_freq_table[i].frequency = data[j];
+    dts_freq_table[i].frequency = CPUFREQ_TABLE_END;
+#endif
+
 	if (ports)
 		devm_kfree(dev, ports);
 
@@ -599,6 +617,22 @@ static int cpufreq_parse_dt(struct device *dev)
 
 	return 0;
 }
+
+#ifdef CONFIG_CPU_VOLTAGE_CONTROL
+bool is_used_by_scaling(unsigned int freq)
+{
+    unsigned int i, cpu_freq;
+    
+    for (i = 0; dts_freq_table[i].frequency != CPUFREQ_TABLE_END; i++) {
+        cpu_freq = dts_freq_table[i].frequency;
+        if (cpu_freq == CPUFREQ_ENTRY_INVALID)
+            continue;
+        if (freq == cpu_freq)
+            return true;
+    }
+    return -EINVAL;
+}
+#endif
 
 #ifdef CONFIG_DEBUG_FS
 static int msm_cpufreq_show(struct seq_file *m, void *unused)

broodROM/android_kernel_huawei_msm8916

drivers/cpufreq/qcom-cpufreq.c
@@ -29,6 +29,10 @@
 #include <linux/of.h>
 #include <trace/events/power.h>
 
+#ifdef CONFIG_CPU_VOLTAGE_CONTROL
+static struct cpufreq_frequency_table *dts_freq_table;
+#endif
+
 static DEFINE_MUTEX(l2bw_lock);
 
 static struct clk *cpu_clk[NR_CPUS];
@@ -334,6 +338,12 @@ static struct cpufreq_frequency_table *cpufreq_parse_dt(struct device *dev,
 	if (nf == 0)
 		return ERR_PTR(-EINVAL);
 
+#ifdef CONFIG_CPU_VOLTAGE_CONTROL
+	dts_freq_table = devm_kzalloc(dev, (nf + 1) * sizeof(struct cpufreq_frequency_table),
+		GFP_KERNEL);
+	if (!dts_freq_table)
+		return ERR_PTR(-ENOMEM);
+#endif
 	data = devm_kzalloc(dev, nf * sizeof(*data), GFP_KERNEL);
 	if (!data)
 		return ERR_PTR(-ENOMEM);
@@ -374,15 +384,38 @@ static struct cpufreq_frequency_table *cpufreq_parse_dt(struct device *dev,
 
 		ftbl[i].driver_data = i;
 		ftbl[i].frequency = f;
+#ifdef CONFIG_CPU_VOLTAGE_CONTROL
+		dts_freq_table[i].frequency = data[i];
+#endif
 	}
 
 	ftbl[i].driver_data = i;
 	ftbl[i].frequency = CPUFREQ_TABLE_END;
-
+#ifdef CONFIG_CPU_VOLTAGE_CONTROL
+	dts_freq_table[i].frequency = CPUFREQ_TABLE_END;
+#endif
 	devm_kfree(dev, data);
 
 	return ftbl;
 }
+
+
+#ifdef CONFIG_CPU_VOLTAGE_CONTROL
+bool is_used_by_scaling(unsigned int freq)
+{
+    unsigned int i, cpu_freq;
+    
+    for (i = 0; dts_freq_table[i].frequency != CPUFREQ_TABLE_END; i++) {
+        cpu_freq = dts_freq_table[i].frequency;
+        if (cpu_freq == CPUFREQ_ENTRY_INVALID)
+            continue;
+        if (freq == cpu_freq)
+            return true;
+    }
+    return -EINVAL;
+}
+#endif
+
 
 static int __init msm_cpufreq_probe(struct platform_device *pdev)
 {
@@ -502,3 +535,4 @@ static int __init msm_cpufreq_early_register(void)
 	return register_hotcpu_notifier(&msm_cpufreq_cpu_notifier);
 }
 core_initcall(msm_cpufreq_early_register);
+